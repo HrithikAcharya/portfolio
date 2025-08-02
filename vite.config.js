4901f0a96f6914342f47f7b19c648cd0804efe64
Hrithik Acharya <hrithikacharya2001@gmail.com>
2025-08-01 21:55:52 -0400

    Final

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  base:"https://github.com/HrithikAcharya/hrithikacharya.github.io/",
+  base:"/hrithikacharya.github.io/",
 })
